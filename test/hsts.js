20e5191f46f23456fc4ff61ae845609bd9f26896
Matt Venables <mattvenables@gmail.com>
2014-03-29 18:26:24 -0400

    Update HSTS for koa

--- test/hsts.js
+++ test/hsts.js
@@ -1,34 +1,39 @@
+'use strict';
+
 var helmet = require('../');
 
-var connect = require('connect');
+var koa = require('koa');
 var request = require('supertest');
 
-describe('hsts', function () {
-    "use strict";
+describe('hsts', function() {
+  var app;
 
-    var app;
-    beforeEach(function () {
-        app = connect();
+  beforeEach(function() {
+    app = koa();
+  });
+
+  it('sets header properly with no args', function(done) {
+    app.use(helmet.hsts());
+    app.use(function *() {
+      this.body = 'Hello world!';
     });
 
-    it('sets header properly with no args', function (done) {
-        app.use(helmet.hsts());
-        app.use(function (req, res) {
-            res.end('Hello world!');
-        });
-        request(app).get('/')
-        .set('x-forwarded-proto', 'https')
-        .expect('Strict-Transport-Security', 'max-age=15768000', done);
+    request(app.listen())
+      .get('/')
+      .set('x-forwarded-proto', 'https')
+      .expect('Strict-Transport-Security', 'max-age=15768000', done);
+  });
+
+  it('sets header properly with args', function(done) {
+    app.use(helmet.hsts('1234', true));
+    app.use(function *() {
+      this.body = 'Hello world!';
     });
 
-    it('sets header properly with args', function (done) {
-        app.use(helmet.hsts('1234', true));
-        app.use(function (req, res) {
-            res.end('Hello world!');
-        });
-        request(app).get('/')
-        .set('x-forwarded-proto', 'https')
-        .expect('Strict-Transport-Security', 'max-age=1234; includeSubdomains', done);
-    });
+    request(app.listen())
+      .get('/')
+      .set('x-forwarded-proto', 'https')
+      .expect('Strict-Transport-Security', 'max-age=1234; includeSubdomains', done);
+  });
 
 });
