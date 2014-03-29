6af7db98eb774fb4d176bd97d7c3c2540ea101dc
Matt Venables <mattvenables@gmail.com>
2014-03-29 18:27:14 -0400

    Update ienoopen for koa

--- test/ienoopen.js
+++ test/ienoopen.js
@@ -1,23 +1,24 @@
 var helmet = require('../');
 
-var connect = require('connect');
+var koa = require('koa');
 var request = require('supertest');
 
-describe('ienoopen', function () {
+describe('ienoopen', function() {
+  var app;
 
-    var app;
-    beforeEach(function () {
-        app = connect();
-        app.use(helmet.ienoopen());
-        app.use(function (req, res) {
-            res.setHeader('Content-Disposition', 'attachment; filename=somefile.txt');
-            res.end('Download this cool file!');
-        });
+  beforeEach(function() {
+    app = koa();
+    app.use(helmet.ienoopen());
+    app.use(function *() {
+      this.set('Content-Disposition', 'attachment; filename=somefile.txt');
+      this.body = 'Download this cool file!';
     });
+  });
 
-    it('sets header properly', function (done) {
-        request(app).get('/')
-        .expect('X-Download-Options', 'noopen', done);
-    });
+  it('sets header properly', function(done) {
+    request(app.listen())
+      .get('/')
+      .expect('X-Download-Options', 'noopen', done);
+  });
 
 });
