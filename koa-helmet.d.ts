f6e5ccbc647e4ab24db06a079c3f8c1c7d8004c9
Julien Wajsberg <felash@gmail.com>
2024-11-20 15:23:25 +0100

    Fix types errors when using with Helmet 7 and 8

--- koa-helmet.d.ts
+++ koa-helmet.d.ts
@@ -2,86 +2,19 @@
 // Project: https://github.com/venables/koa-helmet#readme
 // Definitions by: Nick Simmons <https://github.com/nsimmons>
 //                 Jan Dolezel <https://github.com/dolezel>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+//                 Julien Wajsberg <https://github.com/julienw>
 // TypeScript Version: 2.3
 
-import helmet from "helmet";
-import { Middleware, Context } from "koa";
+import type { default as helmet, HelmetOptions } from "helmet";
+import { Middleware } from "koa";
 
-type HelmetOptions = helmet.HelmetOptions;
+type Helmet = typeof helmet;
 
-declare namespace koaHelmet {
-  type KoaHelmetContentSecurityPolicyDirectiveFunction = (
-    req?: Context["req"],
-    res?: Context["res"],
-  ) => string;
+type KoaHelmet = {
+  [HelmetMiddleware in keyof Helmet]: (
+    ...options: Parameters<Helmet[HelmetMiddleware]>
+  ) => Middleware;
+} & ((options?: HelmetOptions) => Middleware);
 
-  type KoaHelmetCspDirectiveValue =
-    | string
-    | KoaHelmetContentSecurityPolicyDirectiveFunction;
-
-  interface KoaHelmetContentSecurityPolicyDirectives {
-    baseUri?: KoaHelmetCspDirectiveValue[];
-    childSrc?: KoaHelmetCspDirectiveValue[];
-    connectSrc?: KoaHelmetCspDirectiveValue[];
-    defaultSrc?: KoaHelmetCspDirectiveValue[];
-    fontSrc?: KoaHelmetCspDirectiveValue[];
-    formAction?: KoaHelmetCspDirectiveValue[];
-    frameAncestors?: KoaHelmetCspDirectiveValue[];
-    frameSrc?: KoaHelmetCspDirectiveValue[];
-    imgSrc?: KoaHelmetCspDirectiveValue[];
-    mediaSrc?: KoaHelmetCspDirectiveValue[];
-    objectSrc?: KoaHelmetCspDirectiveValue[];
-    pluginTypes?: KoaHelmetCspDirectiveValue[];
-    prefetchSrc?: KoaHelmetCspDirectiveValue[];
-    reportTo?: string;
-    reportUri?: string;
-    sandbox?: KoaHelmetCspDirectiveValue[];
-    scriptSrc?: KoaHelmetCspDirectiveValue[];
-    scriptSrcAttr?: KoaHelmetCspDirectiveValue[];
-    scriptSrcElem?: KoaHelmetCspDirectiveValue[];
-    styleSrc?: KoaHelmetCspDirectiveValue[];
-    styleSrcAttr?: KoaHelmetCspDirectiveValue[];
-    styleSrcElem?: KoaHelmetCspDirectiveValue[];
-    workerSrc?: KoaHelmetCspDirectiveValue[];
-  }
-
-  interface KoaHelmetContentSecurityPolicyConfiguration {
-    reportOnly?: boolean;
-    useDefaults?: boolean;
-    directives?: KoaHelmetContentSecurityPolicyDirectives;
-  }
-
-  interface KoaHelmet {
-    (options?: HelmetOptions): Middleware;
-    contentSecurityPolicy(
-      options?: KoaHelmetContentSecurityPolicyConfiguration,
-    ): Middleware;
-    crossOriginEmbedderPolicy(
-      options?: HelmetOptions["crossOriginEmbedderPolicy"],
-    ): Middleware;
-    crossOriginOpenerPolicy(
-      options?: HelmetOptions["crossOriginOpenerPolicy"],
-    ): Middleware;
-    crossOriginResourcePolicy(
-      options?: HelmetOptions["crossOriginResourcePolicy"],
-    ): Middleware;
-    dnsPrefetchControl(
-      options?: HelmetOptions["dnsPrefetchControl"],
-    ): Middleware;
-    expectCt(options?: HelmetOptions["expectCt"]): Middleware;
-    frameguard(options?: HelmetOptions["frameguard"]): Middleware;
-    hidePoweredBy(options?: HelmetOptions["hidePoweredBy"]): Middleware;
-    hsts(options?: HelmetOptions["hsts"]): Middleware;
-    ieNoOpen(options?: HelmetOptions["ieNoOpen"]): Middleware;
-    noSniff(options?: HelmetOptions["noSniff"]): Middleware;
-    permittedCrossDomainPolicies(
-      options?: HelmetOptions["permittedCrossDomainPolicies"],
-    ): Middleware;
-    referrerPolicy(options?: HelmetOptions["referrerPolicy"]): Middleware;
-    xssFilter(options?: HelmetOptions["xssFilter"]): Middleware;
-  }
-}
-
-declare const koaHelmet: koaHelmet.KoaHelmet;
-export = koaHelmet;
+const koaHelmet: KoaHelmet;
+export default koaHelmet;
